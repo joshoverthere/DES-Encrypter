b7b02cf531d80e1aca6d80fe0bc24497f232471a
joshoverthere <contact@xeta.space>
2022-01-28 22:40:24 +0000

    Add project files.

---
+++ DES_Encrypter.py
@@ -0,0 +1,14 @@
+#DES Rules:
+#not a fkn clue time to read wikipedia
+
+import os
+
+clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+
+def main():
+    while (1):
+        clearConsole()
+        print("Welcome to DES Encrypter!")
+        input()
+
+main()
